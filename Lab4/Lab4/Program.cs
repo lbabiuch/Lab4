331f9b4eac7119a5caf93858d8276f3ee87f4a73
lbabiuch <luki.babiuch@gmail.com>
2019-10-19 13:41:50 +0200

    lab4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = { 1, 2, 3, 4, 5 };
+            int[] nowaTablica = new int[tablica.Length];
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                nowaTablica[i] = tablica[i];
+            }
+            nowaTablica[3] = 10;
+
+            foreach (var liczba in tablica)
+            {
+                Console.WriteLine(liczba + " ");
+            }
+
+            foreach (var liczba in nowaTablica)
+            {
+                Console.WriteLine(liczba + " ");
+            }
+
+        }
+    }
+}
